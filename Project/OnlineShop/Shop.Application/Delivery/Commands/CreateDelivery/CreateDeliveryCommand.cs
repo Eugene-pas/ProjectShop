afad51b0c55ee81f9e2753f806494cf3dfe620c1
tonia4ik-s <cooltone4ka@gmail.com>
2022-02-02 16:00:03 +0200

    DeliveryFolder

---
+++ Project/OnlineShop/Shop.Application/Delivery/Commands/CreateDelivery/CreateDeliveryCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Shop.Application.Delivery.Commands.CreateDelivery
+{
+    public class CreateDeliveryCommand : IRequest<long>
+    {
+        public long Id { get; set; }
+
+    }
+}
