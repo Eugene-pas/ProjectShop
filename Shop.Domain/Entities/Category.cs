b56811846a09b5a83dc01c32eb7af6a3eaa00670
tonia4ik-s <cooltone4ka@gmail.com>
2022-02-03 12:00:50 +0200

    MigrationCategory

--- Shop.Domain/Entities/Category.cs
+++ Shop.Domain/Entities/Category.cs
@@ -10,12 +10,12 @@ namespace Shop.Domain.Entities
     {
         public Category()
         {
-            this.ParentCategory = new HashSet<Category>();
+            //this.ParentCategory = new HashSet<Category>();
             this.Product = new HashSet<Product>();
         }
         public string Name { get; set; }
     
-        public virtual ICollection<Category> ParentCategory { get; set; }
+        public virtual Category ParentCategory { get; set; }
         public virtual ICollection<Product> Product { get; set; }
     }
 }
