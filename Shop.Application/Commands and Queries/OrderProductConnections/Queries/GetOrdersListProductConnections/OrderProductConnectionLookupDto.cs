cdedec4dc447975850b173dae6cc6043464d2082
Eugen <56602775+Eugene-pas@users.noreply.github.com>
2022-02-07 15:17:21 +0200

    fix CRUD for OrderProductConnection

--- Shop.Application/Commands and Queries/OrderProductConnections/Queries/GetOrdersListProductConnections/OrderProductConnectionLookupDto.cs
+++ Shop.Application/Commands and Queries/OrderProductConnections/Queries/GetOrdersListProductConnections/OrderProductConnectionLookupDto.cs
@@ -15,16 +15,17 @@ namespace Shop.Application.Commands_and_Queries.OrderProductConnections.Queries.
         public long Id { get; set; }
         public virtual Order Order { get; set; }
         public virtual Product Product { get; set; }
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<OrderProductConnection, OrderProductConnectionLookupDto>()
+                .ForMember(x => x.Id,
+                opt => opt.MapFrom(x => x.Id))
+                .ForMember(x => x.Product,
+                opt => opt.MapFrom(x => x.Product))
+                .ForMember(x => x.Order,
+                opt => opt.MapFrom(x => x.Order));
+
+        }
     }
-    public void Mapping(Profile profile)
-    {
-        profile.CreateMap<OrderProductConnection, OrderProductConnectionLookupDto>()
-            .ForMember(x => x.Id,
-            opt => opt.MapFrom(x => x.Id))
-            .ForMember(x => x.Product,
-            opt => opt.MapFrom(x => x.Product))
-            .ForMember(x => x.Order,
-            opt => opt.MapFrom(x => x.Order));
-            
-    }
+    
 }
