13703e9001def27efb5c3598f529254f8a635cd2
Eugen <56602775+Eugene-pas@users.noreply.github.com>
2022-02-07 11:33:56 +0200

    Create CRUD for DeliveryProductConnection

---
+++ Shop.Application/Commands and Queries/DeliveryConnections/Commands/CreatDeliveryProductConnection/CreateDeliveryProductConnectionCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Shop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Commands_and_Queries.DeliveryConnections
+{
+    public class CreateDeliveryProductConnectionCommand
+        : IRequest<long>
+    {       
+        public long DeliveryId { get; set; }
+        public long ProductId { get; set; }
+    }
+}
