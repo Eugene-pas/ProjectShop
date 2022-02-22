cb628cab4794bc18877b8dc42d635ce40dddf5a1
reeditor <roman.tymoshchuk@oa.edu.ua>
2022-02-22 17:28:19 +0200

    Delete using

--- Shop.Domain/Entities/Seller.cs
+++ Shop.Domain/Entities/Seller.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using Shop.Domain.Entities.Base;
+using System.Collections.Generic;
 
 namespace Shop.Domain.Entities
 {
