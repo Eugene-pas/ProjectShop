7ff890ca4bde1e652a029c0d3129c300537ea6fb
tonia4ik-s <cooltone4ka@gmail.com>
2022-02-23 12:26:55 +0200

    ProductListByCategory pagination

--- Shop.Application/Commands/Products/Queries/GetProductsListByCategory/GetProductsListByCategoryHandler.cs
+++ Shop.Application/Commands/Products/Queries/GetProductsListByCategory/GetProductsListByCategoryHandler.cs
@@ -1,32 +1,30 @@
 ﻿using AutoMapper;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shop.Application.Commands.Products.Queries.GetProductsListPaginated;
 using Shop.Application.Common;
+using Shop.Application.Common.Pagination;
+using Shop.Application.Interfaces;
 using Shop.Domain.Entities;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Shop.Application.Interfaces;
-using Microsoft.EntityFrameworkCore;
-using Shop.Application.Commands.Products.Queries.GetProductsList;
-using Shop.Application.Common.Pagination;
 
 namespace Shop.Application.Commands.Products.Queries.GetProductsListByCategory
 {
     public class GetProductsListByCategoryHandler
-    : HandlersBase, IRequestHandler<GetProductsListByCategoryQuery, ProductsListVm>
+    : HandlersBase, IRequestHandler<GetProductsListByCategoryQuery, ProductPaginatedVm>
     {
         public GetProductsListByCategoryHandler(IDataBaseContext dbContext, IMapper mapper)
             : base(dbContext, mapper) { }
 
-        public async Task<ProductsListVm> Handle(GetProductsListByCategoryQuery request, CancellationToken cancellationToken)
+        public async Task<ProductPaginatedVm> Handle(GetProductsListByCategoryQuery request, CancellationToken cancellationToken)
         {
-            var listProduct = new List<Product>();
             var listCategories = SubcategoriesFind(_dbContext, request.CategoryId, new List<Category>());
             listCategories.Add(await _dbContext.Category
                 .Include(x => x.Product)
-                .FirstOrDefaultAsync(x => x.Id == request.CategoryId));
+                .FirstOrDefaultAsync(x => x.Id == request.CategoryId, cancellationToken));
             if (listCategories.Count == 0)
             {
                 var category = await _dbContext.Category
@@ -37,15 +35,19 @@ namespace Shop.Application.Commands.Products.Queries.GetProductsListByCategory
                     listCategories.Add(category);
                 }
             }
-            foreach (var category in listCategories)
-            {
-                foreach (var item in category.Product)
-                {
-                    listProduct.Add(item);
-                }
-            }
 
-            return new ProductsListVm{Products = listProduct};
+            var listProduct = listCategories.SelectMany(category => category.Product).AsQueryable();
+            var paginatedList = await PaginatedList<Product>
+                .CreateAsync(listProduct, request.PageNumber, request.PageSize);
+
+            return new ProductPaginatedVm
+            {
+                Products = paginatedList,
+                Page = paginatedList.PageIndex,
+                TotalPagesAmount = paginatedList.TotalPages,
+                HasNextPage = paginatedList.HasNextPage,
+                HasPreviousPage = paginatedList.HasPreviousPage
+            };
         }
     }
 }
